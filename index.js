c250a5b3fbe66f1841e2b58065fa278ef8836f11
Eder Martins Franco <efranco23@gmail.com>
2019-08-30 22:14:17 -0400

    Fix request

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ db.loadDatabase();
 app.use(bodyParser.json());
 app.use(cors());
 
-const MSG_OK  = "OK";
-const MSG_FORMATO_INVALIDO  = "O formato do produto é inválido!";
-const MSG_NAO_ENCONTADO  = "Nenhum produto encontrado";
+const MSG_OK = "{'message': 'OK'}";
+const MSG_FORMATO_INVALIDO = "{'message': 'O formato do produto é inválido!'}";
+const MSG_NAO_ENCONTADO = "{'message': 'Nenhum produto encontrado'}";
 
 // GET ALL
 app.get('/produto', (req, res) => {
     db.find({}).then(produto => {
-        if(!produto || !produto.length) {
+        if (!produto || !produto.length) {
             res.status(404).send(MSG_NAO_ENCONTADO);
             return;
         }
@@ -31,7 +31,7 @@ app.get('/produto', (req, res) => {
 // GET ONE
 app.get('/produto/:id', (req, res) => {
     db.find({ _id: req.params.id }).then(produto => {
-        if(!produto || !produto.length) {
+        if (!produto || !produto.length) {
             res.status(404).send(MSG_NAO_ENCONTADO);
             return;
         }
@@ -41,7 +41,7 @@ app.get('/produto/:id', (req, res) => {
 
 // INSERT
 app.post('/produto', (req, res) => {
-    if(ValidacaoUtils.isProdutoValido(req.body)) {
+    if (ValidacaoUtils.isProdutoValido(req.body)) {
         db.insert(ValidacaoUtils.getCamposValidos(req.body)).then(() => {
             res.send(MSG_OK);
         }).catch(() => res.status(500).end());
@@ -52,13 +52,13 @@ app.post('/produto', (req, res) => {
 
 // UPDATE
 app.put('/produto/:id', (req, res) => {
-    if(ValidacaoUtils.isProdutoValido(req.body)) {
+    if (ValidacaoUtils.isProdutoValido(req.body)) {
         db.update({ _id: req.params.id }, ValidacaoUtils.getCamposValidos(req.body)).then(() => {
             res.send(MSG_OK);
         }).catch(() => res.status(500).end());
     } else {
         res.status(400).send(MSG_FORMATO_INVALIDO);
-    }    
+    }
 });
 
 // DELETE
